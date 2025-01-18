06edde2c7c9367f9645873fb0445c7d8ab760425
nerdoutlook <vincentuyanwanne@gmail.com>
2025-01-18 12:44:19 +0000

    turned on the filter function

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ def fetch_tokens_route():
     tokens = fetch_tokens()
     
     
-    # Load the blacklist
-    #blacklist = load_blacklist() 
-    #filtered_tokens = filter_tokens(tokens, blacklist)
+    #Load the blacklist
+    blacklist = load_blacklist() 
+    filtered_tokens = filter_tokens(tokens, blacklist)
     
     # Check if the filtered tokens list is empty or not
-    #if not filtered_tokens:
-        #print("No tokens after filtering.")
+    if not filtered_tokens:
+        print("No tokens after filtering.")
     
     # Return the filtered tokens to the frontend
-    #return jsonify(filtered_tokens)
+    return jsonify(filtered_tokens)
 
     #skipping the filtering function
     return(tokens)
